f0264611f0cd7982a8cac8707dece7c86b18a8d8
Daniel <skap.tk@gmail.com>
2022-09-30 14:33:20 -0500

    Created and changed to an ASP.NET project structure.

---
+++ CityInfo/Pages/Error.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CityInfo.Pages;
+
+[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+public class ErrorModel : PageModel
+{
+    private readonly ILogger<ErrorModel> _logger;
+
+    public ErrorModel(ILogger<ErrorModel> logger)
+    {
+        _logger = logger;
+***REMOVED***
+
+    public string? RequestId { get; set;***REMOVED***
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+
+    public void OnGet()
+    {
+        RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
+***REMOVED***
+}
+
